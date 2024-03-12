2d42555c6bee80644777e13847703f918c947a5b
mcnuggetsx20 <alilenbaxy@gmail.com>
2024-03-12 22:26:13 +0100

    12 Mar 2024 22:26:11

--- cw/sem2/l2/ArrayFromK.java
+++ cw/sem2/l2/ArrayFromK.java
@@ -1,3 +1,25 @@
-public class ArrayFromK implements Iterator<T>{
-    public class
+import java.util.Iterator;
+public class ArrayFromK<T> implements Iterator<T>{
+    int k = 0;
+    public ArrayFromK(int k){
+        this.k = k;
+    }
+
+    @Override
+    public boolean hasNext(){
+        return true;
+    }
+
+    @Override
+    public T next(){
+        return ++k;
+    }
+
+    public static void main(String[] args){
+        ArrayFromK<Integer> iter = new ArrayFromK<>(5);
+        while(iter.next != 100){
+            System.out.println(iter.next());
+        }
+        return;
+    }
 }
